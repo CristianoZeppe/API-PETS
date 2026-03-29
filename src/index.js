8f63082df56f36d1cfbc91d44572f7a6cc27d41c
Cristiano Luiz Zeppe <Cristianozeppe@gmail.com>
2026-03-28 21:37:18 -0300

    Routes to get, update, and delete a pet

--- src/index.js
+++ src/index.js
@@ -56,6 +56,101 @@ app.post("/pets", (req, res) => {
    }
 });
 
+// Obter um pet pelo id
+// GET/pets:id
+app.get("/pets/:id", (req, res) => {
+   try {
+      // entrada
+      const { id } = req.params;
+
+      // processamento
+      const pet = pets.find((item) => item.id === id);
+      if (!pet) {
+         return res.status(404).send({
+            ok: false,
+            mensagem: "O pet não foi encontrado",
+         });
+      }
+
+      // saída
+      res.status(200).send({
+         ok: true,
+         mensagem: "Pet obtido com sucesso",
+         dados: pet,
+      });
+   } catch (error) {
+      res.status(500).send({
+         ok: false,
+         mensagem: error.toString(),
+      });
+   }
+});
+
+// Atualizar um pey existente
+// PUT/pets/:id
+app.put("/pets/:id", (req, res) => {
+   try {
+      // entrada
+      const { id } = req.params;
+      const { nome, raça, idade, nomeTutor } = req.body;
+
+      // processamento
+      const pet = pets.find((item) => item.id === id);
+      if (!pet) {
+         return res.status(404).send({
+            ok: false,
+            mensagem: "O pet não foi encontrado",
+         });
+      }
+      pet.nome = nome;
+      pet.raça = raça;
+      pet.idade = idade;
+      pet.nomeTutor = nomeTutor;
+
+      // saída
+      res.status(200).send({
+         ok: true,
+         mensagem: "Pet atualizado com sucesso",
+         dados: pets,
+      });
+   } catch (error) {
+      res.status(500).send({
+         ok: false,
+         mensagem: error.toString(),
+      });
+   }
+});
+
+// Excluir um pet
+// DELETE
+app.delete("/pets/:id", (req, res) => {
+   try {
+      // entrada
+      const { id } = req.params;
+
+      // processamento
+      const petIndex = pets.findIndex((item) => item.id === id);
+      if (petIndex < 0) {
+         return res.status(404).send({
+            ok: false,
+            mensagem: "Pet não foi encontrado",
+         });
+      }
+      pets.splice(petIndex, 1);
+      // saída
+      res.status(200).send({
+         ok: true,
+         mensagem: "Pet excluído com sucesso",
+         dados: pets,
+      });
+   } catch (error) {
+      res.status(500).send({
+         ok: false,
+         mensagem: error.toString(),
+      });
+   }
+});
+
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
    console.log(`O servidor está rodando na porta ${port}`);
